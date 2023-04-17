9c1614d9df2b508c578cab2a949053195e564433
Ruan Failache <ruanfailache@gmail.com>
2023-04-17 11:19:59 -0300

    refactor presentation errors

--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,12 +1,7 @@
-// === Forms === //
+export 'errors/form_error.dart';
 export 'forms/sign_in_form.dart';
 export 'forms/sign_up_form.dart';
-// === Helpers === //
-export 'helpers/errors.dart';
-export 'helpers/form_presenter.dart';
-// === Inputs === //
 export 'inputs/email_input.dart';
 export 'inputs/password_input.dart';
-// === Presenters === //
 export 'presenters/get_sign_in_presenter.dart';
 export 'presenters/get_sign_up_presenter.dart';
